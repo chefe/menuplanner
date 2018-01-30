aa711257f6e7a6379edbe8c0cc68568362c952b4
chefe <hi@chefe.io>
2018-01-30 14:18:13 +0100

    Create login and register dusk test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_navigate_to_the_login_page()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->clickLink('Login')
+                ->assertSee('Login')
+                ->assertSee('E-Mail')
+                ->assertSee('Password');
+        });
+    }
+
+    /** @test */
+    public function a_user_can_login()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'john@example.com'
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home')
+                ->assertSee('Dashboard');
+        });
+    }
+}
---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegisterTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_navigate_to_the_login_page()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->clickLink('Register')
+                ->assertSee('Register')
+                ->assertSee('Name')
+                ->assertSee('E-Mail')
+                ->assertSee('Password')
+                ->assertSee('Confirm');
+        });
+    }
+
+    /** @test */
+    public function a_user_can_register_themself()
+    {
+        $user = factory(User::class)->make();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/register')
+                ->type('name', $user->name)
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->type('password_confirmation', 'secret')
+                ->press('Register')
+                ->assertPathIs('/home')
+                ->assertSee('Dashboard');
+        });
+    }
+}
