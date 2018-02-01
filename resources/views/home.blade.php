c73f88503d5656fe42e43d381fe260c973c2ea4b
chefe <hi@chefe.io>
2018-02-01 21:55:51 +0100

    Add fallback screen if the user has disabled javascript

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,10 @@
 @section('content')
 <status></status>
 <router-view></router-view>
+
+<noscript>
+    <div class="flex justify-center text-center items-center text-3xl m-8 p-4 bg-red-lighter h-32 sm:shadow">
+        Please activate javascript, otherwise the application will not work!
+    </div>
+</noscript>
 @endsection
