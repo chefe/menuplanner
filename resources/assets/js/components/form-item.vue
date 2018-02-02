90de5ebe1f590ee7a5330b9dfe368a4b6cc58b19
chefe <hi@chefe.io>
2018-02-02 14:56:30 +0100

    Add form-control css class

--- resources/assets/js/components/form-item.vue
+++ resources/assets/js/components/form-item.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center p-2 flex-col sm:flex-row">
         <label class="w-full sm:w-1/3 mb-0 pr-4 sm:text-right" v-text="caption + ':'"></label>
-        <div class="w-full sm:w-2/3 form-item-form-control">
+        <div class="w-full sm:w-2/3">
             <slot></slot>
         </div>
     </div>
--- resources/assets/js/components/menuplan/form.vue
+++ resources/assets/js/components/menuplan/form.vue
@@ -5,16 +5,17 @@
                     name="title" 
                     v-model="menuplan.title" 
                     placeholder="Please provide a title" 
+                    class="form-control"
                     required />
         </form-item>
         <form-item caption="Start">
-            <input type="date" name="start" v-model="menuplan.start" required />
+            <input type="date" name="start" v-model="menuplan.start" class="form-control" required />
         </form-item>
         <form-item caption="End">
-            <input type="date" name="end" v-model="menuplan.end" required />
+            <input type="date" name="end" v-model="menuplan.end" class="form-control" required />
         </form-item>
         <form-item caption="People">
-            <input type="number" name="people" v-model="menuplan.people" min="1" required />
+            <input type="number" name="people" v-model="menuplan.people" min="1" class="form-control" required />
         </form-item>
         
         <div class="w-full sm:w-2/3 ml-auto flex sm:py-2 sm:pr-2 flex-col sm:flex-row">
