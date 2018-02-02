8319e1d10e2dbf0ba6ba8cf2f07a94844ad72a3a
chefe <hi@chefe.io>
2018-02-02 15:31:06 +0100

    Add delete button for menuplans

--- resources/assets/js/components/center-panel.vue
+++ resources/assets/js/components/center-panel.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex items-center mb-8">
         <div class="w-full mx-auto sm:w-3/4 lg:w-1/2 sm:rounded sm:shadow">
-            <div v-if="hasHeader" class="flex items-center font-medium text-lg bg-brand-light p-3 sm:rounded-t">
+            <div v-if="hasHeader" :class="headerClasses">
                 <slot name="header" class="p-3"></slot>
             </div>
-            <div :class="'bg-white sm:rounded-b ' +(hasHeader ? '' : 'sm:rounded-t')">
+            <div :class="contentClasses">
                 <slot></slot>
             </div>
         </div>
@@ -13,9 +13,14 @@
 
 <script>
     export default {
+        props: ['header-accent'],
         computed: {
+            headerClasses() {
+                return 'flex items-center font-medium text-lg p-3 sm:rounded-t ' +
+                    'bg-' + (this.headerAccent ? this.headerAccent : 'brand') + '-light';
+            },
             contentClasses() {
-
+                return 'bg-white sm:rounded-b ' + (this.hasHeader ? '' : 'sm:rounded-t');
             },
             hasHeader() {
                 return !! this.$slots['header'];
--- resources/assets/js/components/menuplan/edit.vue
+++ resources/assets/js/components/menuplan/edit.vue
@@ -1,12 +1,26 @@
 <template>
-    <center-panel>
-        <template slot="header">Edit Menuplan</template>
-        <menuplan-form 
-            :menuplan="menuplan" 
-            @submit="save"
-            @cancel="cancel">
-        </menuplan-form>
-    </center-panel>
+    <div>
+        <center-panel>
+            <template slot="header">Edit Menuplan</template>
+            <menuplan-form 
+                :menuplan="menuplan" 
+                @submit="save"
+                @cancel="cancel">
+            </menuplan-form>
+        </center-panel>
+        <center-panel header-accent="red">
+            <template slot="header">Delete Menuplan</template>
+            <div v-if="confirmMode" class="px-2 py-4 text-center">
+                <span class="text-grey-dark mr-2">Are you sure?</span>
+                <a class="btn-secondary mr-2" @click="cancelDelete">No</a>
+                <a class="btn-danger" @click="deleteMenuplan">Yes</a>
+            </div>
+            <div v-else class="px-2 py-4">
+                <a class="btn-danger mr-2" @click="deleteMenuplan">Delete Menuplan</a>
+                <span class="text-grey-dark">This action can to be reverted!</span>
+            </div>
+        </center-panel>
+    </div>
 </template>
 
 <script>
@@ -24,7 +38,8 @@
                     end: '',
                     people: 1
                 }, 
-                endpoint: ''
+                endpoint: '',
+                confirmMode: false,
             }
         },
         mounted() {
@@ -59,6 +74,26 @@
             },
             cancel() {
                 router.go(-1);
+            }, 
+            deleteMenuplan() {
+                if (this.confirmMode == false) {
+                    this.confirmMode = true;
+                    return;
+                }
+
+                let that = this;
+                bus.$emit('loading', true);
+                axios.delete(this.endpoint)
+                    .then(function (response) {
+                        bus.$emit('loading', false);
+                        router.push('/');
+                    })
+                    .catch(function (error) {
+                        bus.$emit('error', error);
+                    });
+            },
+            cancelDelete() {
+                this.confirmMode = false;
             }
         }
     }
