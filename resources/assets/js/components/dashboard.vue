18040be5be59424d8ab45fec571903ffd7fda46c
chefe <hi@chefe.io>
2018-02-05 17:34:56 +0100

    Add duartion to dashboard and menuplan show view

--- resources/assets/js/components/dashboard.vue
+++ resources/assets/js/components/dashboard.vue
@@ -8,7 +8,9 @@
         </template>
 
         <div v-for="menuplan in menuplans" class="flex text-sm p-3 border-b items-center" :key="menuplan.id">
-            <router-link :to="'/menuplan/' + menuplan.id" class="flex-1 text-grey-dark hover:text-grey-darkest no-underline" v-text="menuplan.title">
+            <router-link :to="'/menuplan/' + menuplan.id" class="flex-1 text-grey-dark hover:text-grey-darkest no-underline">
+                <span class="mr-2" v-text="menuplan.title"></span>
+                <small class="text-grey" v-text="getDuration(menuplan)"></small>
             </router-link>
             <router-link :to="'/menuplan/' + menuplan.id + '/edit'" class="text-grey-dark hover:text-grey-darkest">
                 <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
@@ -25,6 +27,7 @@
 
 <script>
     import { bus } from '../eventbus.js';
+    import moment from 'moment';
 
     export default {
         data() {
@@ -47,6 +50,10 @@
                     .catch(function (error) {
                         bus.$emit('error', error);
                     });
+            },
+            getDuration(menuplan) {
+                return moment(menuplan.start).format('Do MMM') + 
+                    ' - ' + moment(menuplan.end).format('Do MMM');
             }
         }
     }
--- resources/assets/js/components/menuplan/show.vue
+++ resources/assets/js/components/menuplan/show.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container mx-auto">
         <div class="flex items-center mx-2 mb-8 p-2 rounded border-b-2 shadow-b text-grey-darkest text-xl bg-white">
-            <h1 class="flex-1" v-text="menuplan.title"></h1>
+            <h1 class="flex-1">
+                <span class="mr-2" v-text="menuplan.title"></span>
+                <small class="text-grey-dark" v-text="duration"></small>
+            </h1>
             <router-link :to="'/menuplan/' + $route.params.id + '/items'" class="text-grey-darkest ml-4">
                 <svg class="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 9.87V20H2V9.87a4.25 4.25 0 0 0 3-.38V14h10V9.5a4.26 4.26 0 0 0 3 .37zM3 0h4l-.67 6.03A3.43 3.43 0 0 1 3 9C1.34 9 .42 7.73.95 6.15L3 0zm5 0h4l.7 6.3c.17 1.5-.91 2.7-2.42 2.7h-.56A2.38 2.38 0 0 1 7.3 6.3L8 0zm5 0h4l2.05 6.15C19.58 7.73 18.65 9 17 9a3.42 3.42 0 0 1-3.33-2.97L13 0z"/></svg>
             </router-link>
@@ -46,6 +49,12 @@
             this.endpoint = '/api/menuplan/' + this.$route.params.id;
             this.fetchMenuplan();
             this.fetchMeals();
+        },
+        computed: {
+            duration() {
+                return moment(this.menuplan.start).format('Do MMM') + 
+                    ' - ' + moment(this.menuplan.end).format('Do MMM');
+            }
         },
         methods: {
             fetchMenuplan() {
