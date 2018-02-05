c4e4e1e37789866c7210fedcfbf3ffb8d3a73212
chefe <hi@chefe.io>
2018-02-05 21:09:19 +0100

    Adjust layout of main navigation for mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,12 +18,13 @@
         <nav class="bg-white h-12 shadow mb-8">
             <div class="container mx-auto h-full">
                 <div class="flex items-center justify-center h-12">
-                    <div class="mr-6">
-                        <a href="{{ url('/') }}" class="no-underline">
+                    <div class="mr-6 pl-4">
+                        <a href="{{ url('/') }}" class="no-underline text-grey-darker text-xl">
+                            <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 11v7a2 2 0 0 1-4 0v-5h-2V3a3 3 0 0 1 3-3h3v11zM4 10a2 2 0 0 1-2-2V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v7a2 2 0 0 1-2 2v8a2 2 0 0 1-4 0v-8z"/></svg>
                             {{ config('app.name', 'Menuplanner') }}
                         </a>
                     </div>
-                    <div class="flex-1 text-right">
+                    <div class="flex-1 text-right pr-4">
                         @guest
                             <a class="no-underline hover:underline text-grey-darker pr-3 text-sm" href="{{ url('/login') }}">Login</a>
                             <a class="no-underline hover:underline text-grey-darker text-sm" href="{{ url('/register') }}">Register</a>
