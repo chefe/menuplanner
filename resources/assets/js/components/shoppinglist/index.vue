b70cb9b3095c78ac720dd59e8f2020ade2e991d7
chefe <hi@chefe.io>
2018-02-11 15:38:33 +0100

    Add link to download the pdf version of the shopping list

--- resources/assets/js/components/shoppinglist/index.vue
+++ resources/assets/js/components/shoppinglist/index.vue
@@ -2,6 +2,9 @@
     <div class="container mx-auto">
         <div class="flex items-center mx-2 mb-8 p-2 rounded border-b-2 shadow-b text-grey-darkest text-xl bg-white">
             <h1 class="flex-1">Shopping List</h1>
+            <a :href="'/menuplan/' + $route.params.id + '/shopping-list/pdf'" class="text-grey-darkest ml-4">
+                <svg class="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+            </a>
             <router-link :to="'/menuplan/' + $route.params.id" class="text-grey-darkest ml-4">
                 <svg class="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 0h9v9H0V0zm2 2v5h5V2H2zm-2 9h9v9H0v-9zm2 2v5h5v-5H2zm9-13h9v9h-9V0zm2 2v5h5V2h-5zm-2 9h9v9h-9v-9zm2 2v5h5v-5h-5z"/></svg>
             </router-link>
