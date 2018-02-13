672290df25080a38aa8dc357cc2c265c331ee2b5
chefe <hi@chefe.io>
2018-02-13 14:28:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -17,10 +17,11 @@ class Ingredient extends Model
     {
         return $this->belongsTo(Item::class);
     }
-    
+
     public function getQuantityForShoppingListAttribute()
     {
         $people = ($this->meal->people == null ? $this->meal->menuplan->people : $this->meal->people);
+
         return $this->quantity * ($people / $this->meal->ingredients_for);
     }
 }
