b49e57ae3111a351e00e4b1f299bd8d0d88a7780
chefe <hi@chefe.io>
2018-03-21 18:37:52 +0100

    Add status label to share component

--- resources/assets/js/components/share/index.vue
+++ resources/assets/js/components/share/index.vue
@@ -5,7 +5,10 @@
         </template>
 
         <div v-for="invitation in invitations" class="flex text-sm p-3 border-b items-center" :key="invitation.id">
-            <span class="flex-1 text-grey-dark" v-text="invitation.email"></span>
+            <div class="flex-1 text-grey-dark">
+                <span v-text="invitation.email"></span>
+                <small class="p-1 rounded bg-grey-dark text-white" v-text="getStatus(invitation)"></small>
+            </div>
             <a @click="deleteInvitation(invitation)" class="text-grey-dark hover:text-grey-darkest cursor-pointer">
                 <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
             </a>
@@ -71,6 +74,9 @@
                 axios.delete(endpoint).then(response => {
                     that.fetchInvitations();
                 });
+            },
+            getStatus(invitation) {
+                return invitation.user_id == null ? 'Pending' : 'Accepted';
             }
         }
     }
