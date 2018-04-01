082f09cb5294c6353800458f487b0f615123e432
chefe <hi@chefe.io>
2018-04-01 20:56:33 +0200

    Wrap center component with an extra container div

--- resources/assets/js/components/center-panel.vue
+++ resources/assets/js/components/center-panel.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="flex items-center mb-8">
-        <div class="w-full mx-auto sm:w-3/4 lg:w-1/2 sm:rounded sm:shadow">
-            <div v-if="hasHeader" :class="headerClasses">
-                <slot name="header" class="p-3"></slot>
-            </div>
-            <div :class="contentClasses">
-                <slot></slot>
+    <div class="container mx-auto">
+        <div class="flex items-center mb-8">
+            <div class="w-full mx-auto sm:w-3/4 lg:w-1/2 sm:rounded sm:shadow">
+                <div v-if="hasHeader" :class="headerClasses">
+                    <slot name="header" class="p-3"></slot>
+                </div>
+                <div :class="contentClasses">
+                    <slot></slot>
+                </div>
             </div>
         </div>
     </div>
