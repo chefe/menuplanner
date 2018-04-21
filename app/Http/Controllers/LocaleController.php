1d3b91ba5927fde5e9762775ba40b9bf3b953cf6
chefe <hi@chefe.io>
2018-04-21 21:17:10 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class LocaleController extends Controller
 {
     public function setLocale($locale)
     {
         session()->put('locale', $locale);
+
         return back();
     }
 }
