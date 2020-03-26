b0b9ab1ca27cb1c5c5f9051bca3315b256dced74
Laravel Shift <shift@laravelshift.com>
2020-03-26 11:47:16 +0000

    Shift deprecated Resource class

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ItemResource extends Resource
+class ItemResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Resource::withoutWrapping();
+        JsonResource::withoutWrapping();
     }
 }
