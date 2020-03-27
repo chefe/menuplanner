52c291e8ae63c71b7f37617ad404f2d9f2cdbe9d
chefe <hi@chefe.io>
2020-03-27 07:55:05 +0100

    Add test for locale selection

---
+++ tests/Feature/SetLocaleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SetLocaleTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_set_the_locale_to_german()
+    {
+        $response = $this->get('/locale/de');
+        $response->assertSessionHas('locale', 'de');
+        $response->assertRedirect('/');
+    }
+
+    /** @test */
+    public function a_user_can_set_the_locale_to_english()
+    {
+        $response = $this->get('/locale/en');
+        $response->assertSessionHas('locale', 'en');
+        $response->assertRedirect('/');
+    }
+}
